b436aec69e7830188e388fc30727f651a3577f2c
sherryuser <sherryuser1@gmail.com>
2025-04-16 12:25:07 +0500

    Add google analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from 'next-themes';
 import nProgress from 'nprogress';
 import * as React from 'react';
 import { SWRConfig } from 'swr';
+import Script from 'next/script';
 
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import 'react-tippy/dist/tippy.css';
@@ -15,7 +16,6 @@ import '@/styles/mdx.css';
 import '@/styles/nprogress.css';
 
 import { getFromLocalStorage } from '@/lib/helper.client';
-
 import { blockDomainMeta } from '@/constants/env';
 
 Router.events.on('routeChangeStart', nProgress.start);
@@ -24,16 +24,13 @@ Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
-    // Don't increment views if not on main domain
     if (
       window.location.host !==
-        (process.env.NEXT_PUBLIC_BLOCK_DOMAIN_WHITELIST ||
-          'shaxriyor.live') &&
+        (process.env.NEXT_PUBLIC_BLOCK_DOMAIN_WHITELIST || 'shaxriyor.live') &&
       blockDomainMeta
     ) {
       if (getFromLocalStorage('incrementMetaFlag') !== 'false') {
         localStorage.setItem('incrementMetaFlag', 'false');
-        // reload page to make changes
         window.location.reload();
       }
     }
@@ -42,15 +39,34 @@ function MyApp({ Component, pageProps }: AppProps) {
   useRemoteRefresh();
 
   return (
-    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
-      <SWRConfig
-        value={{
-          fetcher: (url) => axios.get(url).then((res) => res.data),
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-PWGNW4JGM4'
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PWGNW4JGM4');
+          `,
         }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
-    </ThemeProvider>
+      />
+      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+        <SWRConfig
+          value={{
+            fetcher: (url) => axios.get(url).then((res) => res.data),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
+      </ThemeProvider>
+    </>
   );
 }
 
